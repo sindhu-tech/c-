33e268ab11226e7c0e1d6bc98c550defb4a0a623
sindhu-tech <63792468+sindhu-tech@users.noreply.github.com>
2020-05-15 21:34:15 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first, second, sum;
+    
+    cout << "Enter two integers: ";
+    cin >> first >> secondr;
+
+    
+    sum = first + second;
+
+
+    cout << first << " + " <<  second << " = " << sum;     
+
+    return 0;
+}
